54c36f16019aeb20e1d02ba88c3369e51f6cde12
Ivan Indrakusuma <ivan.17051@mhs.its.ac.id>
2020-03-09 14:39:15 +0700

    Add get

--- server.js
+++ server.js
@@ -1,17 +1,30 @@
-const http = require("http");
-const hostname = '10.199.14.46';
+var express = require('express');
+var app = express();
+var mahasiswaController = require('./Controller/MahasiswaController')();
+//const http = require("http");
+const hostname = 'localhost';
 const port = 8011;
 
-//Create HTTP server and listen on port 8020 for requests
-const server = http.createServer((req, res) => {
+// //Create HTTP server and listen on port 8020 for requests
+// const server = http.createServer((req, res) => {
 
-  //Set the response HTTP header with HTTP status and Content type
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hi World\n');
+//   //Set the response HTTP header with HTTP status and Content type
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hi World\n');
+// });
+
+// //listen for request on port 3000, and as a callback function have the port listened on logged
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+app.get("/", function(request, response){
+  response.json({"Message":"Hello"});
 });
+app.use("/mhs", mahasiswaController);
 
-//listen for request on port 3000, and as a callback function have the port listened on logged
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, function(){
+  var message = "Server running on Port: " + port;
+  console.log(message);
 });
